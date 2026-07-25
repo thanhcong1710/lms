c57b79a329a8f19441930793a1f018cdd5b98266
thanhcong1710 <thanhcong1710@gmail.com>
2026-07-25 20:52:21 +0700

    update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -25,7 +25,8 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            $user = \App\Http\Controllers\AuthController::resolveUser($request);
+            return Limit::perMinute(3000)->by($user?->id ?: $request->ip());
         });
 
         $this->routes(function () {
